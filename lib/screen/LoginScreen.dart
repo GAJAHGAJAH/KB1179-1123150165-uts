172a20f6c4f61fa3b2bd41dc3c39202ff270252d
GAJAHGAJAH <1123150165@global.ac.id>
2025-11-08 22:10:11 +0700

    menambahkan icon lock pada LoginScreen

--- lib/screen/LoginScreen.dart
+++ lib/screen/LoginScreen.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class Loginscreen extends StatelessWidget {
+  const Loginscreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: const Column(
+            children: [
+              SizedBox(height: 10),
+              Icon(Icons.lock_outline, size: 80, color: Colors.green),
+              SizedBox(height: 10),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
