a2f191a4c9b3a4882793bc93e74880e144dc7c85
GAJAHGAJAH <1123150165@global.ac.id>
2025-11-08 22:13:06 +0700

    wrap column ke center

--- lib/screen/LoginScreen.dart
+++ lib/screen/LoginScreen.dart
@@ -9,20 +9,22 @@ class Loginscreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Container(
           padding: const EdgeInsets.all(20.0),
-          child: const Column(
-            children: [
-              SizedBox(height: 10),
-              Icon(Icons.lock_outline, size: 80, color: Colors.green),
-              SizedBox(height: 10),
-              Text(
-                "Selamat Datang",
-                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                "“Silakan login untuk melanjutkan”",
-                style: TextStyle(fontSize: 12.0, color: Colors.green),
-              ),
-            ],
+          child: Center(
+            child: const Column(
+              children: [
+                SizedBox(height: 10),
+                Icon(Icons.lock_outline, size: 80, color: Colors.green),
+                SizedBox(height: 10),
+                Text(
+                  "Selamat Datang",
+                  style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "“Silakan login untuk melanjutkan”",
+                  style: TextStyle(fontSize: 12.0, color: Colors.green),
+                ),
+              ],
+            ),
           ),
         ),
       ),
