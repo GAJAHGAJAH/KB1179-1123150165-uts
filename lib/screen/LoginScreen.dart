7050d560736ed2080061883b61cbb9158ff0a7aa
GAJAHGAJAH <1123150165@global.ac.id>
2025-11-08 22:22:50 +0700

    membuat textfield password

--- lib/screen/LoginScreen.dart
+++ lib/screen/LoginScreen.dart
@@ -10,25 +10,39 @@ class Loginscreen extends StatelessWidget {
         child: Container(
           padding: const EdgeInsets.all(20.0),
           child: Center(
-            child: const Column(
+            child: Column(
               children: [
-                SizedBox(height: 10),
-                Icon(Icons.lock_outline, size: 80, color: Colors.green),
-                SizedBox(height: 10),
-                Text(
+                const SizedBox(height: 10),
+                const Icon(Icons.lock_outline, size: 80, color: Colors.green),
+                const SizedBox(height: 10),
+                const Text(
                   "Selamat Datang",
                   style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
                 ),
-                Text(
+                const Text(
                   "“Silakan login untuk melanjutkan”",
                   style: TextStyle(fontSize: 12.0, color: Colors.green),
                 ),
-                TextField(
+                const TextField(
                   keyboardType: TextInputType.emailAddress,
                   decoration: InputDecoration(
                     labelText: 'Email',
                     hintText: 'Masukkan Email Anda',
                     prefixIcon: Icon(Icons.email_outlined),
+                  ),
+                ),
+                TextField(
+                  obscureText: true,
+                  cursorColor: Colors.white,
+                  style: const TextStyle(fontSize: 14),
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    hintText: 'Masukkan Password Anda',
+                    prefixIcon: const Icon(Icons.lock_outline),
+                    suffixIcon: Container(
+                      margin: const EdgeInsets.only(right: 15),
+                      child: const Icon(Icons.visibility_off, size: 20),
+                    ),
                   ),
                 ),
               ],
